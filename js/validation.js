14c693518507f422ca0dcdd730c663f50f10f8a6
Luciano Munhoz Silva <luciano.asimov@gmail.com>
2022-06-08 20:38:01 -0300

    Form validation

---
+++ js/validation.js
@@ -0,0 +1,155 @@
+export function valida (input) {
+    const tipoDeInput = input.dataset.tipo
+
+    if (validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
+
+    if (input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
+    }
+}
+
+const tiposDeErro = [
+	'valueMissing',
+	'typeMismatch',
+	'patternMismatch',
+	'customError'
+]
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'Name field cannot be empty.'
+    },
+    email: {
+        valueMissing: 'Email field cannot be empty.',
+        typeMismatch: 'Email entered is not valid.'
+    }, 
+    company_name: {
+        valueMissing: 'Company name field cannot be empty.'
+    },
+    company_field: {
+        valueMissing: 'Company field cannot be empty.'
+    },       
+    cidade: {
+        valueMissing: 'Company city field cannot be empty.'
+    },
+    estado: {
+        valueMissing: 'Company state field cannot be empty.'
+    },
+    message: {
+        valueMissing: 'Message field cannot be empty.'
+    },
+}
+
+const validadores = {
+    dataNascimento:input => validaDataNascimento(input),
+		cpf:input => validaCPF(input),
+        cep:input => recuperarCEP(input)
+}
+
+function mostraMensagemDeErro(tipoDeInput, input) {
+    let mensagem = ''
+		tiposDeErro.forEach(erro => {
+			if(input.validity[erro]) {
+				mensagem = mensagensDeErro[tipoDeInput][erro]
+			}
+		})
+
+    return mensagem
+}
+
+function validaDataNascimento (input) {
+    const dataRecebida = new Date(input.value)
+    let mensagem = ''
+
+    if (!maiorQue18(dataRecebida)) {
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
+    }
+    
+    input.setCustomValidity(mensagem)
+}
+
+function maiorQue18(data) {
+    const dataAtual = new Date()
+    const dataMais18  = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
+
+    return dataMais18 <= dataAtual
+}
+
+function validaCPF(input) {
+	const cpfFormatado = input.value.replace(/\D/g, '')
+	let mensagem = ''
+
+	if(!checaCPFRepetido(cpfFormatado)) {
+		mensagem = 'O CPF não é válido.'
+	}
+
+	input.setCustomValidity(mensagem)
+}
+
+function checaCPFRepetido(cpf) {
+	const valoresRepetidos = [
+		'00000000000',
+		'11111111111',
+		'22222222222',
+		'33333333333',
+		'44444444444',
+		'55555555555',
+		'66666666666',
+		'77777777777',
+		'88888888888',
+		'99999999999'
+	]
+	let cpfValido = true
+
+	valoresRepetidos.forEach(valor => {
+		if(valor == cpf) {
+			cpfValido = false
+		}
+	})
+
+	return cpfValido
+}
+
+function recuperarCEP(input) {
+    const cep = input.value.replace(/\D/g, '')
+    const url = `https://viacep.com.br/ws/${cep}/json/`
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'content-type': 'application/json;charset=utf-8'
+        }
+    }
+
+    if (!input.validity.patternMismatch && !input.validity.valueMissing) {
+        fetch(url,options).then(
+            response => response.json()
+        ).then(
+            data => {
+                if(data.erro) {
+                    input.setCustomValidity('Não foi possível buscar o CEP.')
+                    return
+                }
+                input.setCustomValidity('')
+                preencheCamposComCEP(data)
+                return
+            }
+        )
+    }
+}
+
+function preencheCamposComCEP(data) {
+    const logradouro = document.querySelector('[data-tipo="logradouro"]')
+    const cidade = document.querySelector('[data-tipo="cidade"]')
+    const estado = document.querySelector('[data-tipo="estado"]')
+
+    logradouro.value = data.logradouro
+    cidade.value = data.localidade
+    estado.value = data.uf
+}
